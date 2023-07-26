1771145669fcabcc1d2cb3036ddc58988fd3be4f
Nate McMurray <87338101+cathepsin@users.noreply.github.com>
2023-07-26 16:06:23 -0600

    Create solution.py

---
+++ Lesson 6 Practice/solution.py
@@ -0,0 +1,48 @@
+import random
+
+def Hangman(number_of_wrong_guesses, seed=None):
+  if seed is not None:
+    random.seed(seed)
+  hidden_word = random_words[random.randrange(0,500)]
+
+  number_of_wrong_guesses = 6
+  wrong_guesses = 0
+  number_of_characters = len(hidden_word)
+  guessed_characters = []
+
+  print(f"Let's play hangman! You get {number_of_wrong_guesses} wrong guesses")
+  print(f"My word is {len(hidden_word)} characters long")
+
+  guess = ""
+  current_word = ""
+  while wrong_guesses < number_of_wrong_guesses and current_word != hidden_word:
+    print()
+    print(f"{number_of_wrong_guesses - wrong_guesses} wrong guesses remaining")
+    print(f"Guesses: {guessed_characters}")
+    print(current_word)
+
+
+    print("What is your guess?")
+    guess = input()
+
+    if guess in guessed_characters:
+      print(f"You already guessed {guess}")
+      continue
+    else:
+      guessed_characters.append(guess)
+      if guess not in hidden_word:
+        print(f"{guess} is not in my word!")
+        wrong_guesses += 1
+      else:
+        print(f"Looks like {guess} is in my word!")
+        current_word = ""
+        for i in range(len(hidden_word)):
+          if hidden_word[i] in guessed_characters:
+            current_word += hidden_word[i]
+          else:
+            current_word += "_"
+
+  if current_word == hidden_word:
+    print("You win!")
+  else:
+    print(f"You lose! My word was \"{hidden_word}\"")
